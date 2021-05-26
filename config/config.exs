ae6d3927ca86f6132d1c87fbb39066423e3740ed
matskohe <81321793+matskohe@users.noreply.github.com>
2021-05-26 14:29:12 +0900

    add cors_plug

--- config/config.exs
+++ config/config.exs
@@ -26,6 +26,10 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# Configures CorsPlug https://github.com/mschae/cors_plug
+config :cors_plug,
+  origin: ["http://localhost:3000/test"]
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
--- lib/phoenix_try_web/endpoint.ex
+++ lib/phoenix_try_web/endpoint.ex
@@ -48,5 +48,6 @@ defmodule PhoenixTryWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
+  plug CORSPlug
   plug PhoenixTryWeb.Router
 end
