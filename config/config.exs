86a8a6d181685fe3bcc6d95ea3633c10951ba72e
matskohe <81321793+matskohe@users.noreply.github.com>
2021-05-26 15:44:57 +0900

    no config

--- config/config.exs
+++ config/config.exs
@@ -28,7 +28,7 @@ config :phoenix, :json_library, Jason
 
 # Configures CorsPlug https://github.com/mschae/cors_plug
 # config :cors_plug,
-#   origin: ["https://post-api.vercel.app/test"]
+#   origin: ["https://post-api.vercel.app"]
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
